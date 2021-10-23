1c0ba481a15a792a620b21268529d48ca46d7d69
Mohamad Salimi <mohamad.salimi@hotmail.com>
2021-10-23 14:49:55 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+let btn = document.querySelectorAll(".faq");
+btn.forEach(ele => {
+    ele.addEventListener("click", (e) => {
+        if(ele.classList.contains("active")){
+            ele.classList.remove("active");
+        }else{
+            btn.forEach((ele) => {
+                ele.classList.remove("active");
+            })
+            e.currentTarget.classList.toggle("active");
+        }
+    })
+})
